979e65ec7396766c5995e6f95f27dcf3ff066a00
Vasile Popescu <elisescu@elisescu.com>
2015-01-14 21:18:31 -0600

    temp commit: add minimal libev implementation
    Change-Id: I7cb768c16abb844f51e0a2d677583eb0734d73d0

--- src/ev.c
+++ src/ev.c
@@ -6,3 +6,82 @@
  */
 
 
+#include "ev.h"
+#include <stdlib.h>
+
+typedef struct list {
+    struct list *next;
+    ev_io *io;
+} list_t;
+
+#define LIST(l) ((list_t*)(l))
+#define LIST_INSERT(list, eviodata) { \
+    do { \
+        list_t *el = LIST(list)->next == NULL ? LIST(list) : (list_t *)malloc(sizeof(list_t)); \
+        el->io = eviodata; \
+        el->next = LIST(list); \
+        list = el; \
+    } while (0);\
+}
+
+static list_t first_el = {
+        .next = NULL,
+        .io = NULL
+};
+
+struct ev_loop EV_DEFAULT_S = {
+        .running = 0,
+        .ev_ios = (void *) &first_el
+};
+
+struct ev_loop *EV_DEFAULT = &EV_DEFAULT_S;
+
+// PUBLIC API functions
+int ev_io_init(ev_io *io, io_callbacks io_cb, int fd, int flag) {
+    ev_vb("Init ev_io with fd = %d", fd);
+    if (fd > FD_SETSIZE || fd < 1) {
+        ev_error("invalid file descriptor");
+        return EV_ERROR;
+    }
+    io->cb = io_cb;
+    io->fd = fd;
+    return EV_OK;
+}
+
+void ev_io_start(struct ev_loop *loop, ev_io *io) {
+    ev_vb("Starting ev_io with fd = %d", io->fd);
+    LIST_INSERT(loop->ev_ios, io);
+}
+
+void ev_io_stop(struct ev_loop *loop, ev_io *io) {
+    ev_vb("Stopping ev_io with fd = %d", io->fd);
+    //loop->ev_ios[io->fd] = NULL;
+}
+
+void ev_run(struct ev_loop *loop, int flags) {
+    ev_vb("Starting the loop");
+    fd_set active_fd_set, read_fd_set;
+    int i;
+
+    pthread_mutex_init(&loop->mutex, NULL);
+    FD_ZERO (&active_fd_set);
+    loop->running = 1;
+
+    while (1) {
+        pthread_mutex_lock(&loop->mutex);
+        int r = loop->running;
+        pthread_mutex_unlock(&loop->mutex);
+        if (!r) {
+            ev_vb("Stopping the loop...");
+            break;
+        }
+
+    }
+}
+
+void ev_break(struct ev_loop *loop, int how) {
+    ev_vb("Stopping the loop");
+    pthread_mutex_lock(&loop->mutex);
+    loop->running = 0;
+    pthread_mutex_unlock(&loop->mutex);
+}
--- src/ev.h
+++ src/ev.h
@@ -8,14 +8,56 @@
 #ifndef EV_H_
 #define EV_H_
 
+#ifdef _WIN32
+#include <winsock2.h>
+#include <windows.h>
+#else
+#include <sys/socket.h>
+#endif
+#include <stdio.h>
+#include <pthread.h>
+
+#define ev_error(...)    fprintf(stderr, "\n"__VA_ARGS__)
+#define ev_info(...)     fprintf(stdout, "\n"__VA_ARGS__)
+#define ev_vb(...)       fprintf(stdout, "\n"__VA_ARGS__)
+
+enum {
+    EV_OK = 0,
+    EV_ERROR = 1
+};
+
+enum {
+    EVBREAK_ALL = 0
+};
+
+enum {
+    EV_READ = 1,
+    EV_WRITE = 2,
+    EV_READ_WRITE = EV_READ | EV_WRITE
+};
+
+extern struct ev_loop *EV_DEFAULT;
+
+struct ev_io;
+typedef void (*io_callbacks)(struct ev_loop *loop, struct ev_io *w, int revents);
+
 typedef struct ev_io {
-    int nothing_now;
+    int flags;
+    void *data;
+    int fd;
+    io_callbacks cb;
 } ev_io;
 
 struct ev_loop {
-    int nothing_now;
+    int running;
+    pthread_mutex_t mutex;
+    void *ev_ios;
 };
 
-void ev_io_stop(struct ev_loop *loop, ev_io w);
+int ev_io_init(ev_io *io, io_callbacks io_cb, int fd, int flag);
+void ev_io_start(struct ev_loop *loop, ev_io *io);
+void ev_io_stop(struct ev_loop *loop, ev_io *w);
+void ev_run(struct ev_loop *loop, int flags);
+void ev_break(struct ev_loop *loop, int how);
 
 #endif /* EV_H_ */
